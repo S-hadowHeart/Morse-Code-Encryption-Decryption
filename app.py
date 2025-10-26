ea7f5ea14d8c4305a21ad69f5b2e6c8265b5b339
sodhadharmraj30 <sodhadharmraj30@gmail.com>
2025-10-26 10:41:33 +0000

    fix bug

--- app.py
+++ app.py
@@ -9,31 +9,37 @@ def index():
 
 @app.route('/code', methods=['POST'])
 def code():
-    text = request.form['textInput']
+    text = request.form['textInput'].strip()
     
     if text.startswith(".") or text.startswith("-") or text.startswith("💙") or text.startswith("💜"):
         result = decode(text)
-    else :
+    else:
         result = encode(text)
     return render_template('index.html', result=result)
 
 def encode(a):
-    words = a.split(' ')
-    encoded_words = []
-    for word in words:
-        encoded_word = m(word).stringToMorse()
-        encoded_word = encoded_word.replace(".", "💜").replace("-", "💙")
-        encoded_words.append(encoded_word)
-    return " / ".join(encoded_words)
+    try:
+        words = a.split(' ')
+        encoded_words = []
+        for word in words:
+            encoded_word = m(word).stringToMorse()
+            encoded_word = encoded_word.replace(".", "💜").replace("-", "💙")
+            encoded_words.append(encoded_word)
+        return " / ".join(encoded_words)
+    except KeyError as e:
+        return f"Error: Invalid character for encoding: {e}"
 
 def decode(a):
-    words = a.split(' / ')
-    decoded_words = []
-    for word in words:
-        decoded_word = word.replace("💜", ".").replace("💙", "-")
-        decoded_word = m(decoded_word.strip()).morseToString()
-        decoded_words.append(decoded_word)
-    return " ".join(decoded_words)
+    try:
+        words = a.split(' / ')
+        decoded_words = []
+        for word in words:
+            decoded_word = word.replace("💜", ".").replace("💙", "-")
+            decoded_word = m(decoded_word.strip()).morseToString()
+            decoded_words.append(decoded_word)
+        return " ".join(decoded_words)
+    except KeyError as e:
+        return f"Error: Invalid Morse code sequence: {e}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000, debug=True)
