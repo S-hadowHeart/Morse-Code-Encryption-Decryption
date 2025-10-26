d15d4da60b356253531b1f694bc908f41f4adef6
sodhadharmraj30 <sodhadharmraj30@gmail.com>
2025-10-26 10:26:30 +0000

    improve

--- app.py
+++ app.py
@@ -18,14 +18,22 @@ def code():
     return render_template('index.html', result=result)
 
 def encode(a):
-    encoded_text = m(a).stringToMorse()
-    encoded_text = encoded_text.replace(".", "💜").replace("-", "💙")
-    return encoded_text
+    words = a.split(' ')
+    encoded_words = []
+    for word in words:
+        encoded_word = m(word).stringToMorse()
+        encoded_word = encoded_word.replace(".", "💜").replace("-", "💙")
+        encoded_words.append(encoded_word)
+    return " / ".join(encoded_words)
 
 def decode(a):
-    decoded_text = a.replace("💜", ".").replace("💙", "-")
-    decoded_text = m(decoded_text).morseToString()
-    return decoded_text
+    words = a.split(' / ')
+    decoded_words = []
+    for word in words:
+        decoded_word = word.replace("💜", ".").replace("💙", "-")
+        decoded_word = m(decoded_word.strip()).morseToString()
+        decoded_words.append(decoded_word)
+    return " ".join(decoded_words)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000, debug=True)
